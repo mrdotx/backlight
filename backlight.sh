ef9d93996bec5b2b36ac603fddf6c5c85dad05b8
mrdotx <klassiker@gmx.de>
2020-09-12 15:30:30 +0200

    error handling optimized

--- backlight.sh
+++ backlight.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/backlight/backlight.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/backlight
-# date:       2020-09-12T09:48:18+0200
+# date:       2020-09-12T14:47:31+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to change intel backlight
@@ -11,8 +11,8 @@ help="$script [-h/--help] -- script to change intel backlight
     $script [-inc/-dec] [percent]
 
   Settings:
-    [-inc]    = increase in percent (0-100%)
-    [-dec]    = decrease in percent (0-100%)
+    [-inc]    = increase in percent (0-100)
+    [-dec]    = decrease in percent (0-100)
     [percent] = how much percent to increase/decrease the brightness
 
   Examples:
@@ -24,38 +24,43 @@ if [ ! "$(id -u)" = 0 ]; then
    exit 1
 fi
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ -z "$2" ] || [ $# -eq 0 ]; then
-    printf "%s\n" "$help"
-    exit 0
-else
-    max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-    actual=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
+brightness() {
+    if [ "$1" -le 100 ]; then
+        max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+        actual=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
 
-    if [ "$2" -le 100 ] ; then
-        percent=$((100 / $2))
+        percent=$((100 / $1))
+        factor=$((max / percent))
+        max=$((factor * percent))
     else
         printf "%s\n" "$help"
-        exit 0
+        exit 1
     fi
-    factor=$((max / percent))
-    max=$((factor * percent))
+}
 
-    unset value
-    case "$1" in
-        -inc)
-            value=$((actual + factor))
-            if [ $value -ge $max ]; then
-                value=$max
-            fi
-            ;;
-        -dec)
-            value=$((actual - factor))
-            if [ $value -le 0 ]; then
-                value=0
-            fi
-            ;;
-    esac
+case "$1" in
+    -h | --help)
+        printf "%s\n" "$help"
+        ;;
+    -inc)
+        brightness "$2"
+        value=$((actual + factor))
+        if [ $value -ge $max ]; then
+            value=$max
+        fi
+        ;;
+    -dec)
+        brightness "$2"
+        value=$((actual - factor))
+        if [ $value -le 0 ]; then
+            value=0
+        fi
+        ;;
+    *)
+        printf "%s\n" "$help"
+        exit 1
+        ;;
+esac
 
-    # set brighness
-    printf "%s" "$value" > "/sys/class/backlight/intel_backlight/brightness"
-fi
+# set brighness
+printf "%s" "$value" > "/sys/class/backlight/intel_backlight/brightness"
